af6fe55641f213df0c3b99fd8f5157518a3357df
AngusWorkRecord <angushonwork@gmail.com>
2026-08-10 16:35:15 +0800

    Update URLs in index.html and manifest.json for root path, and adjust icon source path in config-navigation.js

--- src/layouts/dashboard/nav/config-navigation.js
+++ src/layouts/dashboard/nav/config-navigation.js
@@ -8,7 +8,7 @@ import SvgColor from '../../../components/svg-color';
 // ----------------------------------------------------------------------
 
 const icon = (name) => (
-  <SvgColor src={`/BESarawak/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
 );
 
 const ICONS = {
